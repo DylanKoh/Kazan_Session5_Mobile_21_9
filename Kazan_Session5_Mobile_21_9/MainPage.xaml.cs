86abfe44f63610373673bbd278eed3b28d2b9239
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-09-21 22:09:36 +0800

    Fully Functioning Online Offline

--- Kazan_Session5_Mobile_21_9/MainPage.xaml.cs
+++ Kazan_Session5_Mobile_21_9/MainPage.xaml.cs
@@ -28,8 +28,7 @@ namespace Kazan_Session5_Mobile_21_9
         {
             base.OnAppearing();
             checkConnectivity();
-            await LoadPicker();
-            
+
         }
 
         private void checkConnectivity()
@@ -45,22 +44,46 @@ namespace Kazan_Session5_Mobile_21_9
             catch (Exception)
             {
 
-                throw;
+                Title = "An error occured while attempting to connect to DataBase";
             }
 
         }
 
-        private void checkNetwork()
+        private async void checkNetwork()
         {
             if (Connectivity.NetworkAccess == NetworkAccess.Internet)
             {
                 isConnected = true;
                 Title = "Connected to DataBase";
+                btnAdd.IsEnabled = true;
+                btnEdit.IsEnabled = true;
+                if (pWell.SelectedItem != null)
+                {
+                    var previousSelected = pWell.SelectedItem.ToString();
+                    await LoadPicker();
+                    pWell.SelectedItem = previousSelected;
+                }
+                else
+                {
+                    await LoadPicker();
+                }
             }
             else if (Connectivity.NetworkAccess == NetworkAccess.None)
             {
                 isConnected = false;
                 Title = "Disconnected to DataBase";
+                btnAdd.IsEnabled = false;
+                btnEdit.IsEnabled = false;
+                if (pWell.SelectedItem != null)
+                {
+                    var previousSelected = pWell.SelectedItem.ToString();
+                    await LoadPicker();
+                    pWell.SelectedItem = previousSelected;
+                }
+                else
+                {
+                    await LoadPicker();
+                }
             }
         }
 
@@ -74,11 +97,19 @@ namespace Kazan_Session5_Mobile_21_9
                 var wellResponse = await client.PostAsync(null, "Wells");
                 _wellList = JsonConvert.DeserializeObject<List<Well>>(wellResponse);
                 File.WriteAllText(FileSystem.AppDataDirectory + "/wellPicker.txt", wellResponse);
-                
+
             }
             else
             {
-                _wellList = JsonConvert.DeserializeObject<List<Well>>(File.ReadAllText(FileSystem.AppDataDirectory + "/wellPicker.txt"));
+                if (File.Exists(FileSystem.AppDataDirectory + "/wellPicker.txt"))
+                {
+                    _wellList = JsonConvert.DeserializeObject<List<Well>>(File.ReadAllText(FileSystem.AppDataDirectory + "/wellPicker.txt"));
+                }
+                else
+                {
+                    await DisplayAlert("Loading Fields", "Please connect to the internet to retrieve latest copy of data!", "Ok");
+                }
+                
             }
             foreach (var item in _wellList)
             {
@@ -97,63 +128,134 @@ namespace Kazan_Session5_Mobile_21_9
                                where x.WellName == pWell.SelectedItem.ToString()
                                select x).FirstOrDefault();
                 lblCapacity.Text = $"{getWell.Capacity} m^3";
-                var client = new WebApi();
-                var wellLayersResponse = await client.PostAsync(null, $"WellLayers/GetWellLayers?WellID={getWell.ID}");
-                _viewList = JsonConvert.DeserializeObject<List<GridView>>(wellLayersResponse);
-                foreach (var item in _viewList)
+                if (isConnected)
                 {
-                    double getProportion = (Convert.ToDouble(item.End - item.Start) / Convert.ToDouble(getWell.GasOilDepth)) * 400;
+                    var client = new WebApi();
+                    var wellLayersResponse = await client.PostAsync(null, $"WellLayers/GetWellLayers?WellID={getWell.ID}");
+                    _viewList = JsonConvert.DeserializeObject<List<GridView>>(wellLayersResponse);
+                    File.WriteAllText(FileSystem.AppDataDirectory + $"/{getWell.WellName}.txt", wellLayersResponse);
+                    foreach (var item in _viewList)
+                    {
+                        double getProportion = (Convert.ToDouble(item.End - item.Start) / Convert.ToDouble(getWell.GasOilDepth)) * 400;
 
 
-                    var stackLayout1 = new StackLayout()
+                        var stackLayout1 = new StackLayout()
+                        {
+                            Orientation = StackOrientation.Horizontal,
+                            HeightRequest = getProportion,
+                            BackgroundColor = Color.FromHex(item.BackgroundColour)
+                        };
+                        var stackLayout2 = new StackLayout() { Orientation = StackOrientation.Vertical };
+                        var nameLabel = new Label()
+                        {
+                            Text = item.RockName,
+                            HorizontalOptions = LayoutOptions.CenterAndExpand,
+                            VerticalOptions = LayoutOptions.CenterAndExpand,
+                        };
+                        var startLabel = new Label()
+                        {
+                            Text = item.Start.ToString(),
+                            HorizontalOptions = LayoutOptions.End,
+                            VerticalOptions = LayoutOptions.Start
+                        };
+                        var endLabel = new Label()
+                        {
+                            Text = item.End.ToString(),
+                            HorizontalOptions = LayoutOptions.End,
+                            VerticalOptions = LayoutOptions.EndAndExpand
+                        };
+                        stackLayout2.Children.Add(startLabel);
+                        stackLayout2.Children.Add(endLabel);
+                        stackLayout1.Children.Add(nameLabel);
+                        stackLayout1.Children.Add(stackLayout2);
+                        stackView.Children.Add(stackLayout1);
+
+
+                    }
+                    double getOilGasProportion = (Convert.ToDouble(getWell.GasOilDepth - _viewList.Last().End) / Convert.ToDouble(getWell.GasOilDepth)) * 400;
+                    var gasOilStack = new StackLayout()
                     {
                         Orientation = StackOrientation.Horizontal,
-                        HeightRequest = getProportion,
-                        BackgroundColor = Color.FromHex(item.BackgroundColour)
+                        HeightRequest = getOilGasProportion,
+                        BackgroundColor = Color.Black
                     };
-                    var stackLayout2 = new StackLayout() { Orientation = StackOrientation.Vertical };
-                    var nameLabel = new Label()
+                    var gasOilLabel = new Label()
                     {
-                        Text = item.RockName,
+                        Text = "Oil/Gas",
                         HorizontalOptions = LayoutOptions.CenterAndExpand,
                         VerticalOptions = LayoutOptions.CenterAndExpand,
+                        TextColor = Color.White
                     };
-                    var startLabel = new Label()
-                    {
-                        Text = item.Start.ToString(),
-                        HorizontalOptions = LayoutOptions.End,
-                        VerticalOptions = LayoutOptions.Start
-                    };
-                    var endLabel = new Label()
-                    {
-                        Text = item.End.ToString(),
-                        HorizontalOptions = LayoutOptions.End,
-                        VerticalOptions = LayoutOptions.EndAndExpand
-                    };
-                    stackLayout2.Children.Add(startLabel);
-                    stackLayout2.Children.Add(endLabel);
-                    stackLayout1.Children.Add(nameLabel);
-                    stackLayout1.Children.Add(stackLayout2);
-                    stackView.Children.Add(stackLayout1);
-
-
+                    gasOilStack.Children.Add(gasOilLabel);
+                    stackView.Children.Add(gasOilStack);
                 }
-                double getOilGasProportion = (Convert.ToDouble(getWell.GasOilDepth - _viewList.Last().End) / Convert.ToDouble(getWell.GasOilDepth)) * 400;
-                var gasOilStack = new StackLayout()
+                else
                 {
-                    Orientation = StackOrientation.Horizontal,
-                    HeightRequest = getOilGasProportion,
-                    BackgroundColor = Color.Black
-                };
-                var gasOilLabel = new Label()
-                {
-                    Text = "Oil/Gas",
-                    HorizontalOptions = LayoutOptions.CenterAndExpand,
-                    VerticalOptions = LayoutOptions.CenterAndExpand,
-                    TextColor = Color.White
-                };
-                gasOilStack.Children.Add(gasOilLabel);
-                stackView.Children.Add(gasOilStack);
+                    if (File.Exists(FileSystem.AppDataDirectory + $"/{getWell.WellName}.txt"))
+                    {
+                        _viewList = JsonConvert.DeserializeObject<List<GridView>>(File.ReadAllText(FileSystem.AppDataDirectory + $"/{getWell.WellName}.txt"));
+                        foreach (var item in _viewList)
+                        {
+                            double getProportion = (Convert.ToDouble(item.End - item.Start) / Convert.ToDouble(getWell.GasOilDepth)) * 400;
+
+
+                            var stackLayout1 = new StackLayout()
+                            {
+                                Orientation = StackOrientation.Horizontal,
+                                HeightRequest = getProportion,
+                                BackgroundColor = Color.FromHex(item.BackgroundColour)
+                            };
+                            var stackLayout2 = new StackLayout() { Orientation = StackOrientation.Vertical };
+                            var nameLabel = new Label()
+                            {
+                                Text = item.RockName,
+                                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                                VerticalOptions = LayoutOptions.CenterAndExpand,
+                            };
+                            var startLabel = new Label()
+                            {
+                                Text = item.Start.ToString(),
+                                HorizontalOptions = LayoutOptions.End,
+                                VerticalOptions = LayoutOptions.Start
+                            };
+                            var endLabel = new Label()
+                            {
+                                Text = item.End.ToString(),
+                                HorizontalOptions = LayoutOptions.End,
+                                VerticalOptions = LayoutOptions.EndAndExpand
+                            };
+                            stackLayout2.Children.Add(startLabel);
+                            stackLayout2.Children.Add(endLabel);
+                            stackLayout1.Children.Add(nameLabel);
+                            stackLayout1.Children.Add(stackLayout2);
+                            stackView.Children.Add(stackLayout1);
+
+
+                        }
+                        double getOilGasProportion = (Convert.ToDouble(getWell.GasOilDepth - _viewList.Last().End) / Convert.ToDouble(getWell.GasOilDepth)) * 400;
+                        var gasOilStack = new StackLayout()
+                        {
+                            Orientation = StackOrientation.Horizontal,
+                            HeightRequest = getOilGasProportion,
+                            BackgroundColor = Color.Black
+                        };
+                        var gasOilLabel = new Label()
+                        {
+                            Text = "Oil/Gas",
+                            HorizontalOptions = LayoutOptions.CenterAndExpand,
+                            VerticalOptions = LayoutOptions.CenterAndExpand,
+                            TextColor = Color.White
+                        };
+                        gasOilStack.Children.Add(gasOilLabel);
+                        stackView.Children.Add(gasOilStack);
+                    }
+                    else
+                    {
+                        await DisplayAlert("Loading Fields", "Please connect to the internet to retrieve latest copy of data!", "Ok");
+                    }
+                }
+                
+                
             }
 
         }
