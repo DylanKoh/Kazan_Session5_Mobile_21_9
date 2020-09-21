6e2b7c8c501187fa0f27dc7e7014f1474e9293b3
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-09-21 10:37:02 +0800

    Added in GlobalClass for usage

---
+++ Kazan_Session5_Mobile_21_9/GlobalClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kazan_Session5_Mobile_21_9
+{
+    public class GlobalClass
+    {
+        public class RockType
+        {
+            public long ID { get; set; }
+            public string Name { get; set; }
+            public string BackgroundColor { get; set; }
+        }
+
+        public class Well
+        {
+            public long ID { get; set; }
+            public long WellTypeID { get; set; }
+            public string WellName { get; set; }
+            public long GasOilDepth { get; set; }
+            public long Capacity { get; set; }
+        }
+        public class WellLayer
+        {
+            public long ID { get; set; }
+            public long WellID { get; set; }
+            public long RockTypeID { get; set; }
+            public long StartPoint { get; set; }
+            public long EndPoint { get; set; }
+        }
+        public class WellType
+        {
+            public long ID { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
